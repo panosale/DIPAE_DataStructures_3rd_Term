c7e2c09188157b633e31f32957db5df79bbeb77f
panosale <62485609+panosale@users.noreply.github.com>
2023-10-30 14:58:21 +0200

    Update LaboratoryCourse.java

--- Askisi1/src/LaboratoryCourse.java
+++ Askisi1/src/LaboratoryCourse.java
@@ -1,5 +1,30 @@
 // Άσκηση 1.2
 public class LaboratoryCourse {
-  private int labsNumber;
+    private String courseName;
+    private int labsNumber = 5;
+    public LaboratoryCourse() {
+        // Default constructor
+    }  
+    public LaboratoryCourse(String newCourseName, int newLabsNumber) {
+        // Full constructor
+        this.CourseName = newCourseName;
+        this.labsNumber = newLabsNumber;
+    }
   
+    // Setters-Getters
+    public void setCourseName(String newCourseName) {
+        this.courseName = newCourseName;
+    }
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    public void setLabsNumber(int newLabsNumber) {
+        this.labsNumber = newLabsNumber;
+    }
+
+    public int getLabsNumber() {
+        return this.labsNumber;
+    }
+    
 }
