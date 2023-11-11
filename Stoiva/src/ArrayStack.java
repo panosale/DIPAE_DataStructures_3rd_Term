e88018e9eed7d137f5581de0f0f30f02d12a8c32
panosale <panosalevr@hotmail.gr>
2023-11-11 17:53:55 +0200

    Create ArrayStack.java

---
+++ Stoiva/src/ArrayStack.java
@@ -0,0 +1,24 @@
+public class ArrayStack implements Stack{
+    public int getStackSize() {
+        // Επιστρέφει το μέγεθος της Στοίβας
+        return 0; // TO CHANGE
+    }
+    public boolean stackIsEmpty() {
+        // Επιστρέφει true αν η Στοίβα είναι κενή
+        return false; // TO CHANGE
+    }
+    public Object topStackElement() throws StackEmptyException {
+        // Επιστρέφει το στοιχείο που βρίσκεται στην κορυφή της Στοίβας
+        Object tmp = new Object(); // TO CHANGE
+        return tmp; // TO CHANGE
+    }
+    public void pushStackElement(Object item) throws StackFullException {
+        // Εισάγει ένα νέο στοιχείο στην κορυφή της Στοίβας
+    }
+    public Object popStackElement() throws StackEmptyException {
+        // Εξάγει και επιστρέφει το στοιχείο που βρίσκεται στην κορυφή της Στοίβας
+        Object tmp = new Object(); // TO CHANGE
+        return tmp; // TO CHANGE
+    }
+
+}
