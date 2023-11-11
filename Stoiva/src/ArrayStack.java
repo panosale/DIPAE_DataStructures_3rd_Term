c372e6e8079e061b016aa0aea9221b44a80cd993
panosale <panosalevr@hotmail.gr>
2023-11-11 18:02:11 +0200

    Update ArrayStack.java

--- Stoiva/src/ArrayStack.java
+++ Stoiva/src/ArrayStack.java
@@ -1,4 +1,19 @@
+import javax.swing.plaf.PanelUI;
+
 public class ArrayStack implements Stack{
+    public static final int MAX_CAPACITY = 100;
+    private int stackCapacity;
+    private Object[] S;
+    private int top = -1;
+    // Constructor
+    public ArrayStack() {
+        this(MAX_CAPACITY);
+    }
+    // Full comstructor
+    public ArrayStack(int newCapacity) {
+        this.stackCapacity = newCapacity;
+        S = new Object[this.stackCapacity];
+    }
     public int getStackSize() {
         // Επιστρέφει το μέγεθος της Στοίβας
         return 0; // TO CHANGE
