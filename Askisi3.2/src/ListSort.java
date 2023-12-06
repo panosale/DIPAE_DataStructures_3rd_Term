ac8390aa7a79a64281f8373c8acce33a8b818b92
panosale <panosalevr@hotmail.gr>
2023-12-06 19:27:58 +0200

    .

---
+++ Askisi3.2/src/ListSort.java
@@ -0,0 +1,23 @@
+public class ListSort extends LinkedList {
+    public ListSort() {
+        super();
+    }
+    public LinkedList sortList() {
+        Node traceNode, currentNode, minNode;
+        traceNode = this.getFirstNode();
+        while (traceNode != null) {
+            currentNode = traceNode;
+            minNode = traceNode;
+            while (currentNode != null) {
+                if ((currentNode.getItem()).compareTo(minNode.getItem()) < 0) // Έλεγχος εδώ
+                    minNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+            Object temp = traceNode.getItem(); // Έλεγχος εδώ
+            traceNode.setItem(minNode.getItem());
+            minNode.setItem(temp);
+            traceNode = traceNode.getNext();
+        }
+        return this;
+    }
+}
