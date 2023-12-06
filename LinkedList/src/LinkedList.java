0697590ca2da2b9f3c9b4fd1a095eb55d1a491a0
panosale <panosalevr@hotmail.gr>
2023-12-06 19:09:27 +0200

    .

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,9 +1,9 @@
-public class LinkedList {
+public class LinkedList implements List {
     // Υλοποίηση μιας απλά Συνδεδεμένης Λίστας (linked list)
     private static final String MSG_LIST_EMPTY = "Η λίστα είναι κενή!"; // Δήλωση σταθεράς μηνύματος κενής λίστας
     private Node firstNode, lastNode;
     public LinkedList() { // Default constructor
-        this.lastNode = this.lastNode = null;
+        this.firstNode = this.lastNode = null;
     }
     public Node getFirstNode() {
         return this.firstNode;
@@ -12,7 +12,11 @@ public class LinkedList {
         return this.lastNode;
     }
     public boolean isEmpty() {
-        return (this.firstNode == null);
+        return (this.firstNode == null); // Επιστρέφει true (η λίστα είναι κενή) αν ο πρώτος κόμβος είναι null
+    }
+    @Override
+    public int getSize() {
+        return 0;
     }
     public void insertFirst(Object newItem) {
         if (this.isEmpty())
@@ -20,22 +24,39 @@ public class LinkedList {
         else
             this.firstNode = new Node(newItem, this.firstNode);
     }
-    public void insertLas(Object newItem) {
+    public void insertLast(Object newItem) {
         if (this.isEmpty())
             this.firstNode = this.lastNode = new Node(newItem, null);
         else
-            this.lastNode = this.lastNode.next = new Node(newItem, this.firstNode);
+            this.lastNode = this.lastNode.next = new Node(newItem, null);
     }
     public Object removeFirst() throws ListEmptyException {
         if (this.isEmpty())
             throw new ListEmptyException(MSG_LIST_EMPTY);
         Object removeItem = this.firstNode.item;
-        if (this.firstNode == this.lastNode) // Μπορεί να χρησιμοποιηθεί .equals??
+        //if (this.firstNode == this.lastNode)
+        if (this.firstNode.equals(this.lastNode)) // Με την .equals. Για δοκιμή
             this.firstNode = this.lastNode = null;
         else
             this.firstNode = this.firstNode.next;
         return removeItem;
     }
+    public Object removeLast() throws ListEmptyException {
+        if (this.isEmpty())
+            throw new ListEmptyException(MSG_LIST_EMPTY);
+        Object removeItem = this.lastNode.item;
+        if (this.firstNode == lastNode)
+            this.firstNode = this.lastNode = null;
+        else {
+            Node currentNode = this.firstNode;
+            while (currentNode.next != this.lastNode)
+                currentNode = currentNode.next;
+            this.lastNode = currentNode;
+            currentNode.next = null;
+        }
+        return removeItem;
+    }
+
     public void printList() {
         if (this.isEmpty())
             System.out.println(MSG_LIST_EMPTY); // Γιατί όχι το? -> throw new ListEmptyException(MSG_LIST_EMPTY);
