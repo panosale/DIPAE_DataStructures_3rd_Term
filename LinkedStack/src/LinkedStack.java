742251022ac8cd78dd374bd9bf83e6b2a3b865ca
panosale <62485609+panosale@users.noreply.github.com>
2023-12-18 13:35:40 +0200

    Update LinkedStack.java

--- LinkedStack/src/LinkedStack.java
+++ LinkedStack/src/LinkedStack.java
@@ -6,26 +6,23 @@ public class LinkedStack implements Stack {
         S = new LinkedList();
     } // End of: Default constructor
     public boolean stackIsEmpty() {
+        // Ή διαφορετικά: public boolean isEmpty()
         // Επιστρέφει true αν η Συνδεδεμένη Στοίβα είναι κενή
         return S.isEmpty(); // Επιστρέφει true (η λίστα είναι κενή) αν ο πρώτος κόμβος είναι null
     } // End of function: isEmpty()
     public int getStackSize() {
+        // Ή διαφορετικά: public int size()
         // Επιστρέφει το μέγεθος της Συνδεδεμένης Στοίβας
         return S.getSize();
     } // End of function: getSize()
-    public Object topStackElement() throws StackEmptyException {
-        // Επιστρέφει το στοιχείο που βρίσκεται στην κορυφή της Συνδεδεμένης Στοίβας
-        if (S.isEmpty())
-            throw new StackEmptyException((MSG_STACK_EMPTY));
-        Object temp = S.removeFirst();
-        S.insertFirst(temp);
-        return temp;
-    } // End of function: topStackElement()
     public void pushStackElement(Object item) throws StackFullException {
-        // Εισάγει ένα νέο στοιχείο στην κορυφή της Συνδεδεμένης Στοίβας
+        // Ή διαφορετικά: public void push(Item item)
+        // Εισαγωγή/Ώθηση ενός νέου στοιχείου στην κορυφή της Συνδεδεμένης Στοίβας
         S.insertFirst(item);
     } // End of function: pushStackElement()
     public Object popStackElement() throws StackEmptyException {
+        // Ή διαφορετικά: public Item pop()
+        // Διαγραφή/απώθηση του στοιχείου της κορυφής στης Συνδεδεμένης Στοίβας
         try {
             return S.removeFirst();
         }
@@ -33,4 +30,13 @@ public class LinkedStack implements Stack {
             throw new StackEmptyException(MSG_STACK_EMPTY);
         }
     } // End of function: popStackElement()
+    public Object topStackElement() throws StackEmptyException {
+        // Ή διαφορετικά: public Item top()
+        // Επιστρέφει το στοιχείο που βρίσκεται στην κορυφή της Συνδεδεμένης Στοίβας
+        if (S.isEmpty())
+            throw new StackEmptyException((MSG_STACK_EMPTY));
+        Object temp = S.removeFirst();
+        S.insertFirst(temp);
+        return temp;
+    } // End of function: topStackElement()
 }
