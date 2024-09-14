ccfa2f5b52d1504c7162ba6900f1eb5848fb6ebb
panosale <panosalevr@hotmail.gr>
2024-09-15 00:09:12 +0300

    .

---
+++ _CommonStaff/Heap/src/HeapEmptyException.java
@@ -0,0 +1,5 @@
+public class HeapEmptyException extends RuntimeException {
+    public HeapEmptyException(String msg) {
+        super(msg);
+    }
+}
---
+++ _CommonStaff/Heap/src/Main.java
@@ -0,0 +1,6 @@
+// ΥΛΟΠΟΙΗΣΗ ΣΩΡΟΥ ΜΕ ΣΧΕΔΟΝ ΠΛΗΡΕΣ ΔΕΝΤΡΟ
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\nToday we are implementing and testing the Heap Data Structure.");
+    }
+}
