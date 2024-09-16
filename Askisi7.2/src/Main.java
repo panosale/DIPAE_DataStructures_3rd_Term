a920a50a7593ae6d355f8ee06630893d40c46770
panosale <panosalevr@hotmail.gr>
2024-09-17 00:41:18 +0300

    .

--- Askisi7.2/src/Main.java
+++ Askisi7.2/src/Main.java
@@ -48,8 +48,8 @@ public class Main {
                     int input = data.readInt();
                     count++;
                     System.out.print(input + ", ");
-                    }
-                } catch (EOFException eof) {
+                }
+            } catch (EOFException eof) {
                 buff.close();
                 System.out.println("\nIntegers read: " + count);
             }
@@ -58,10 +58,10 @@ public class Main {
             System.out.println("Error: " + error.toString());
         }
     }
-    public static void writeBufferedIntsToFile(int maxpin, String fname) {
+    public static void writeBufferedIntsToFile(int maxpin, String fname, int step) {
         int[] matrix = new int[maxpin];
         for (int i = 0; i < matrix.length; i++)
-            matrix[i] = i * 20;
+            matrix[i] = i * step;
         try {
             FileOutputStream file = new FileOutputStream(fname);
             BufferedOutputStream buff = new BufferedOutputStream(file);
@@ -117,13 +117,71 @@ public class Main {
             System.out.println("Error: " + error.toString());
         }
     }
+    public static void mergeFiles(String inp_f1, String inp_f2, String out_f) {
+        try {
+            FileInputStream inp_file1 = new FileInputStream(inp_f1);
+            DataInputStream inp_data1 = new DataInputStream(inp_file1);
+            FileInputStream inp_file2 = new FileInputStream(inp_f2);
+            DataInputStream inp_data2 = new DataInputStream(inp_file2);
+            FileOutputStream out_file = new FileOutputStream(out_f);
+            DataOutputStream out_data = new DataOutputStream(out_file);
+            int f1_data = 0, f2_data = 0;
+            try {
+                f1_data = inp_data1.readInt();
+                f2_data = inp_data2.readInt();
+                while (true) {
+                    if (f1_data < f2_data) {
+                        out_data.writeInt(f1_data);
+                        System.out.println(f1_data + " from file1 ");
+                        f1_data = inp_data1.readInt();
+                    }
+                    else {
+                        out_data.writeInt(f2_data);
+                        System.out.println(f2_data + " from file2 ");
+                        f2_data = inp_data2.readInt();
+                    }
+                }
+            } catch (EOFException eof) {
+                if (inp_data1.available() > 0) {
+                    inp_data2.close();
+                    System.out.println(" exit file2 ");
+                    try {
+                        while (true) {
+                            out_data.writeInt(f1_data);
+                            System.out.println(f1_data + " from file1 ");
+                            f1_data = inp_data1.readInt();
+                        }
+                    } catch (EOFException eof1) {
+                        inp_data1.close();
+                        out_data.close();
+                    }
+                }
+                else {
+                    inp_data1.close();
+                    System.out.println(" exit file1 ");
+                    try {
+                        while (true) {
+                            out_data.writeInt(f2_data);
+                            System.out.println(f2_data + " from file2 ");
+                            f2_data = inp_data2.readInt();
+                        }
+                    } catch (EOFException eof1) {
+                        inp_data2.close();
+                        out_data.close();
+                    }
+                }
+            }
+        } catch (IOException error) {
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!\nΑΣΚΗΣΗ ΠΡΑΞΗΣ 7.1");
-        writeBytesToFile("./File_with_bytes.dat");
-        readBytesFromFile("./File_with_bytes.dat");
-        writeIntsToFile(100, "./File_with_Integers.dat");
-        writeBufferedIntsToFile(100, "./File_with_Integers.dat"); // Πολύ πιο γρήγορη εγγραφή με τη χρήση της κλάσης BufferedInputStream
-        readIntsFromFile("./File_with_Integers.dat");
-        readBufferedIntsFromFile("./File_with_Integers.dat"); // Πολύ πιο γρήγορη ανάγνωση με τη χρήση της κλάσης BufferedInputStream
+        System.out.println("Hello world!\nΑΣΚΗΣΗ ΠΡΑΞΗΣ 7.2");
+        writeBufferedIntsToFile(20, "./Integers1_file.dat", 2);
+        writeBufferedIntsToFile(20, "./Integers2_file.dat", 3);
+        readBufferedIntsFromFile("./Integers1_file.dat");
+        readBufferedIntsFromFile("./Integers2_file.dat");
+        mergeFiles("./Integers1_file.dat", "./Integers2_file.dat", "./Integers_merged_file.dat");
+        readBufferedIntsFromFile("./Integers_merged_file.dat");
     }
 }
