217bd9234f75700dd16c0acdb2da19b1ce85f17f
panosale <panosalevr@hotmail.gr>
2024-09-16 22:49:38 +0300

    .

--- _CommonStaff/Heap/src/Main.java
+++ _CommonStaff/Heap/src/Main.java
@@ -2,18 +2,28 @@
 public class Main {
     public static boolean heapSort(Object[] array) {
         Heap tranfserHeap = new Heap(array.length);
-        try {
-            for (int i = 0; i < array.length; i++)
-                tranfserHeap.insert(array[i]);
-            int size = tranfserHeap.size();
-            for (int i = size - 1; i >= 0; i--)
-                array[i] = tranfserHeap.remove();
-            return true;
-        }
-        catch (HeapFullException Error) {
-            System.out.println("Ο πίνακας είναι πολύ μεγάλος. Η ταξινόμηση είναι αδύνατη.");
+        if (array.length == 0) {
+            System.out.println("Ο πίνακας που δόθηκε είναι κενός. Η ταξινόμηση είναι αδύνατη.");
             return false;
         }
+        for (int i = 0; i < array.length; i++)
+            tranfserHeap.insert(array[i]);
+        int size = tranfserHeap.size();
+        for (int i = size - 1; i >= 0; i--)
+            array[i] = tranfserHeap.remove();
+        return true;
+//        try {
+//            for (int i = 0; i < array.length; i++)
+//                tranfserHeap.insert(array[i]);
+//            int size = tranfserHeap.size();
+//            for (int i = size - 1; i >= 0; i--)
+//                array[i] = tranfserHeap.remove();
+//            return true;
+//        }
+//        catch (HeapFullException Error) {
+//            System.out.println("Ο πίνακας είναι πολύ μεγάλος. Η ταξινόμηση είναι αδύνατη.");
+//            return false;
+//        }
     } // End of function: public heapSort()
 
     public static void main(String[] args) {
@@ -22,11 +32,12 @@ public class Main {
                 "Πάνος", "Νικολέτα", "Γεώργιος", "Ελένη", "Μαρία", "Λουκάς", "Κωνσταντίνος",
                 "Αλεξάνδρα", "Μαρία", "Νικόλαος", "Φοίβος", "Απόστολος"
         };
+//        String matrix[] = {};
         System.out.println("*** UNSORTED matrix ARRAY ***");
         for (int i = 0; i < matrix.length; i++)
             System.out.println(matrix[i] + ", ");
         if (heapSort(matrix)) {
-            System.out.println("*** UNSORTED matrix ARRAY ***");
+            System.out.println("*** SORTED matrix ARRAY ***");
             for (int i = 0; i < matrix.length; i++)
                 System.out.println(matrix[i] + ", ");
 
